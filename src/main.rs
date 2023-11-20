fe91b0080a9570bb279b51f109d4e8788dc21e3f
anylots <842567494@qq.com>
2023-11-20 14:07:26 +0800

    transfer

--- src/main.rs
+++ src/main.rs
@@ -44,29 +44,17 @@ async fn run() {
         .await;
     println!("tx count: {:?}", count.unwrap());
 
-    let mut token_vec = Vec::<Token<SignerMiddleware<Provider<Http>, _>>>::new();
     let mut i: i32 = 0;
     while i < 10 {
         i += 1;
         let mut rng = rand::thread_rng();
         let wallet = Wallet::new(&mut rng).with_chain_id(chain_id);
-        let tx = TransactionRequest::new()
+        let tx: TransactionRequest = TransactionRequest::new()
             .to(wallet.address())
             .value(1 * 10u64.pow(18));
-        l2_signer.send_transaction(tx, None).await.unwrap();
+        let pending_tx = l2_signer.send_transaction(tx, None).await.unwrap();
         std::thread::sleep(Duration::from_secs(2));
 
-        //Prepare balance
-        let tx1 = token.transfer(wallet.address(), U256::from(10000)).legacy();
-        let rt: Result<_, _> = tx1.send().await;
-        let pending_tx = match rt {
-            Ok(pending_tx) => pending_tx,
-            Err(e) => {
-                println!("prepare fail: {:?}", e);
-                continue;
-            }
-        };
-        std::thread::sleep(Duration::from_secs(2));
         println!(
             "==========>Prepare balance block_number:{:?}",
             l2_provider.get_block_number().await.unwrap()
@@ -93,58 +81,8 @@ async fn run() {
                 // continue;
             }
         }
-
-        let balance = token.balance_of(wallet.address()).await.unwrap();
-        println!("balance: {:?}", balance);
-        assert!(balance == U256::from(10000), "Balance is not as expected");
-
-        let singer = Arc::new(SignerMiddleware::new(l2_provider.clone(), wallet));
-        let token_ts: Token<SignerMiddleware<Provider<Http>, _>> =
-            Token::new(Address::from_str(token_address.as_str()).unwrap(), singer);
-        token_vec.push(token_ts);
     }
 
-    // token_vec.spl
-    println!("waiting for start erc20 tx");
-    std::thread::sleep(Duration::from_secs(10));
-    println!("current time: {:?}", SystemTime::now());
-    println!(
-        "==========>block_number_start:{:?}",
-        l2_provider.get_block_number().await.unwrap()
-    );
-
-    for i in 1..200 {
-        for chunk in token_vec.chunks(2) {
-            let mut handles: Vec<JoinHandle<()>> = Vec::new();
-            for token_ts in chunk.to_owned() {
-                let handle = tokio::spawn(async move {
-                    let tx = token_ts
-                        .transfer(Address::random(), U256::from(1))
-                        .gas(60000)
-                        .legacy();
-                    let rt = tx.send().await;
-                    match rt {
-                        Ok(info) => println!("tx success"),
-                        Err(e) => println!("tx fail: {:?}", e),
-                    }
-                });
-                handles.push(handle);
-            }
-            std::thread::spawn(|| async {
-                for h in handles {
-                    h.await.unwrap();
-                }
-            });
-            println!("chunk start");
-            std::thread::sleep(Duration::from_millis(500));
-        }
-
-        println!(
-            "===========epoch: {:?} complete, block_number: {:?}",
-            i,
-            l2_provider.get_block_number().await.unwrap()
-        );
-    }
     println!(
         "==========>block_number_end:{:?}",
         l2_provider.get_block_number().await.unwrap()
