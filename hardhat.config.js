69cce62ada12bb962ab25c24729a9d8d04698d54
anylots <842567494@qq.com>
2023-11-20 15:27:11 +0800

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     local: {
-      url: `http://localhost:6688`,
-      accounts: ['1212121212121212121212121212121212121212121212121212121212121212']
+      url: `http://localhost:8545`,
+      accounts: ['ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
     }
   }
 };
